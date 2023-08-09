1322bd4549d1d4eac5942620bae5b4ccf7f3f039
Roba-guru <Roba-guru>
2023-08-09 13:24:48 -0700

    updates

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,8 +1,8 @@
 #include "list.h"
 
 /**
- * check_cycle - checks if a linked list contains a cycle
- * @list: linked list to check
+ * check_cycle - checks if a list contains a cycle
+ * @list: linked list to be checked
  *
  * Return: 1 if the list has a cycle, 0 if it doesn't
  */
